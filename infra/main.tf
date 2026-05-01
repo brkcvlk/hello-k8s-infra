58ef70a1447e574e2639cab4de70d5299ae266cf
Burak Civelek <burakcivelekk61@gmail.com>
2026-05-01 16:20:06 +0300

    Add ArgoCD via Helm and Helm provider

--- infra/main.tf
+++ infra/main.tf
@@ -6,11 +6,27 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = ">= 2.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
+  }
 }
 
 data "aws_availability_zones" "available" {
